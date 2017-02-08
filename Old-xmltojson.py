d0415ee86619dbc6206e02e47cc9617eb7c25fe5
eduardohfreitas <duh.henrike@gmail.com>
2017-02-08 09:52:58 -0200

    tratamento para o tipo do campo com o postprocessor

--- Old-xmltojson.py
+++ Old-xmltojson.py
@@ -1,3 +1,24 @@
+
+from lxml import objectify
+
+def xml_to_dict_recursion(xml_object):
+    dict_object = xml_object.__dict__
+    if not dict_object:
+        return xml_object
+    for key, value in dict_object.items():
+        dict_object[key] = xml_to_dict_recursion(value)
+    return dict_object
+
+def xml_to_dict(xml_str):
+    return xml_to_dict_recursion(objectify.fromstring(xml_str))
+
+xml_string = """<?xml version="1.0" encoding="UTF-8"?><Response><NewOrderResp>
+<IndustryType>Test</IndustryType><SomeData><SomeNestedData1>a1234</SomeNestedData1>
+<SomeNestedData2>3455</SomeNestedData2></SomeData></NewOrderResp></Response>"""
+
+print xml_to_dict(xml_string)
+
+'''
 #import xml2json
 import xmltodict
 import optparse
@@ -69,3 +90,4 @@ else:
 
 
 print stringJsonReturn
+'''
--- xmltojson.py
+++ xmltojson.py
@@ -2,7 +2,7 @@
 import json, xmltodict, sys, optparse, xml.etree.cElementTree as ET
 
 # validar numero de parametros
-if len(sys.argv) < 6:
+if len(sys.argv) < 5:
     print "Numero de parametro esta incorreto"
     exit(1)
 
@@ -17,20 +17,33 @@ stringJsonReturn = ''
 # atribuicoes dos parametros
 for i in range(len(sys.argv)):
     if i == 1:
-        pc_xml = sys.argv[i]
+            pc_xml = sys.argv[i]
     elif i == 2:
-        pc_debug = sys.argv[i]
-    elif i == 3:
         if sys.argv[i] == '1':
             pc_ignRootElement = True
-    elif i == 4:
+    elif i == 3:
         pc_changeSeparetorLabel = sys.argv[i]
-    elif i == 5:
+    elif i == 4:
         pc_force_list = sys.argv[i]
+
+
+def postprocessor(path, key, value):
+    try:
+        return key, int(value)
+    except (ValueError, TypeError):
+        try:
+            return key, float(value)
+        except (ValueError, TypeError):
+            if value == 'True':
+                return True
+            elif value == 'False':
+                return False
+            else:
+                return key, value
 
 # converte o xml em dict e o dict em json
 def convert(pc_xml, xml_attribs=False):
-    d = xmltodict.parse(pc_xml,xml_attribs=xml_attribs,force_list=pc_force_list)
+    d = xmltodict.parse(pc_xml,xml_attribs=xml_attribs,postprocessor=postprocessor,force_list=pc_force_list)
     return json.dumps(d,indent=4)
 
 # chama a conversao do xml
